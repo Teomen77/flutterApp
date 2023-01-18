c4209dc5ba312fa7a28058921a6012ed405d3638
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2023-01-18 02:49:21 +0100

    Basic debug UI with list and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -10,47 +10,80 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Applock',
       theme: ThemeData(
         primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'AppLock'),
+      home: const HomePage(title: 'AppLock'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  final List<String> appNames =
+      List<String>.generate(100, (index) => "App #$index");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('This is text at the top'),
-            ListView(
-              padding: const EdgeInsets.all(8.0),
-              children: <Widget>[
-                const Text('Item A'),
-                const Text('Item B'),
-                const Text('Item C'),
-              ],
-            )
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            Expanded(child: buildAppList()),
+            Row(children: [
+              ElevatedButton(onPressed: () {}, child: const Text("Lock")),
+              ElevatedButton(onPressed: () {}, child: const Text("Unlock"))
+            ])
           ],
+        ));
+  }
+
+  ListView buildAppList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      shrinkWrap: true,
+      itemCount: appNames.length,
+      itemBuilder: (context, index) {
+        return AppListItem(
+          appName: appNames[index],
+        );
+      },
+    );
+  }
+}
+
+class AppListItem extends StatelessWidget {
+  const AppListItem({super.key, required this.appName});
+
+  final String appName;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.blueGrey,
+        child: Text(appName[0]),
+      ),
+      title: Text(
+        appName,
+        style: const TextStyle(
+          color: Colors.black54,
         ),
       ),
+      onTap: () {
+        print("$appName clicked!");
+      },
     );
   }
 }
