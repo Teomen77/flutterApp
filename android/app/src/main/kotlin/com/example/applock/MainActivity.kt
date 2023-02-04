b55c9b74a3277de1c4bb2e027771c1296035a07a
TheCreator69 <16518527+TheCreator69@users.noreply.github.com>
2023-02-04 02:57:59 +0100

    Platform implementation for fetching installed packages

--- android/app/src/main/kotlin/com/example/applock/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/applock/MainActivity.kt
@@ -9,6 +9,8 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.BatteryManager
@@ -29,6 +31,10 @@ class MainActivity: FlutterActivity() {
                     result.error("UNAVAILABLE", "Battery level not available", null)
                 }
             }
+            else if(call.method == "getInstalledApps") {
+                val installedApps = getInstalledApps()
+                result.success(installedApps)
+            }
             else {
                 result.notImplemented()
             }
@@ -38,15 +44,40 @@ class MainActivity: FlutterActivity() {
     private fun getBatteryLevel(): Int {
         val batteryLevel: Int
         if(VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            val batteryManager = getSystemService(android.content.Context.BATTERY_SERVICE) as android.os.BatteryManager
-            batteryLevel = batteryManager.getIntProperty(android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY)
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
         }
         else {
-            var intent = android.content.ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(
-                android.content.Intent.ACTION_BATTERY_CHANGED))
-            batteryLevel = intent!!.getIntExtra(android.os.BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
-                android.os.BatteryManager.EXTRA_SCALE, -1)
+            var intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(
+                BatteryManager.EXTRA_SCALE, -1)
         }
         return batteryLevel
+    }
+
+    private fun getInstalledApps(): List<Map<String, Any?>> {
+        val packageManager = activity.packageManager as PackageManager
+
+        // TODO: Implement check for Tiramisu SDK version and use non-deprecated getInstalledApplications()
+        var appList = packageManager.getInstalledApplications(0)
+        appList = appList.filter { app -> !isSystemApp(packageManager, app.packageName) }
+
+        var mappedList = appList.map {app -> appToMap(packageManager, app)}
+        val comp = Comparator {appA: HashMap<String, Any?>, appB: HashMap<String, Any?> -> appA["name"].toString().lowercase().compareTo(appB["name"].toString().lowercase())}
+        mappedList = mappedList.sortedWith(comp)
+
+        return mappedList
+    }
+
+    private fun isSystemApp(packageManager: PackageManager, packageName: String): Boolean {
+        return packageManager.getLaunchIntentForPackage(packageName) == null
+    }
+
+    private fun appToMap(packageManager: PackageManager, app: ApplicationInfo): HashMap<String, Any?> {
+        val appMap = HashMap<String, Any?>()
+        appMap["name"] = packageManager.getApplicationLabel(app)
+        appMap["package_name"] = app.packageName
+        return appMap
     }
 }
